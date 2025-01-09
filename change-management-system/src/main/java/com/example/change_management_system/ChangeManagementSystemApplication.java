12cf8561231a85b901c7647d421f858cc14f5c1a
Cristian Camilo Henao Restrepo <cristian.henao@cadena.com.co>
2025-01-09 16:20:25 -0500

    Initial commit

---
+++ change-management-system/src/main/java/com/example/change_management_system/ChangeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.change_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ change-management-system/src/test/java/com/example/change_management_system/ChangeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.change_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
