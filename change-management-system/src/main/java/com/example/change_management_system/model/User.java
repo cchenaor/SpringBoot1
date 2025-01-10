008be57412e94604d689ed415fd8595dde803cd4
cchenaor <cchenaor@eafit.edu.co>
2025-01-10 20:51:38 +0000

    Ajustes User

--- change-management-system/src/main/java/com/example/change_management_system/model/User.java
+++ change-management-system/src/main/java/com/example/change_management_system/model/User.java
@@ -1,3 +1,9 @@
+package com.example.change_management_system.model;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 @Entity
 public class User {
     @Id
