9909e4898d8a12f9c9392c7d39c4a210627647d3
cchenaor <cchenaor@eafit.edu.co>
2025-01-20 16:04:06 +0000

    Reiniciar

--- change-management-system/src/main/java/com/example/change_management_system/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.change_management_system.Repository;
-
-import com.example.change_management_system.model.User;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
