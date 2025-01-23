bdcf5b8ea2dfb9befeb285f1a7867b527c6a2a26
cchenaor <cchenaor@eafit.edu.co>
2025-01-23 21:36:09 +0000

    UñtimoCommit 23/01

--- HolaMundo/src/main/java/com/example/demo/Controller/HomeController.java
+++ HolaMundo/src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.example.demo.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "Bienvenido a la página de inicio";
+        return "login.html";
     }
 }
